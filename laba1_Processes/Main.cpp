0323daaf2abb62fb9f7dcd0aba28ffa3f369b0bc
Elizaveta Pyalova <93152706+LizaPyalova@users.noreply.github.com>
2022-10-31 11:32:52 +0300

    Add files via upload

---
+++ laba1_Processes/Main.cpp
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+
+wchar_t* convert(const char* elem)
+{
+    wchar_t* c_elem = new wchar_t[strlen(elem)];
+    mbstowcs(c_elem, elem, strlen(elem));
+    return c_elem;
+}
+int main()
+{
+    setlocale(LC_ALL, ".1251");
+    std::string name_of_file;
+    int number_emp;
+    std::cout << "Write name of file :\n";
+    std::cin >> name_of_file;
+    std::cout << "Write number of employees :\n";
+    std::cin >> number_emp;
+    std::string creator = "Creator.exe " + name_of_file + " " + std::to_string(number_emp);
+    wchar_t* elem = convert(creator.c_str());
+    STARTUPINFO si;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    PROCESS_INFORMATION pi;
+    if (!CreateProcess(NULL, elem, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    {
+        std::cerr << "ERROR";
+        return 0;
+    }
+    delete[] elem;
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+
+    std::string file;
+    double salary;
+    std::cout << "Write name of file :\n";
+    std::cin >> file;
+    std::cout << "Write salary per hour :\n";
+    std::cin >> salary;
+    std::string reporter = "Reporter.exe " + name_of_file + " " + file + " " + std::to_string(salary);
+    elem = convert(reporter.c_str());
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    if (!CreateProcess(NULL, elem, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    {
+        std::cerr << "ERROR";
+        return 0;
+    }
+    delete[] elem;
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+
+    std::ifstream fin(file);
+    std::string str;
+    while (std::getline(fin, str)) {
+        std::cout << str << std::endl;
+    }
+}
---
+++ laba1_Processes/Reporter.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct employee
+{
+    int num;
+    char name[10];
+    double hours;
+};
+
+int main(int argc, char* argv[])
+{
+    employee emp;
+    ifstream fin(argv[0]);
+    ofstream fout(argv[2]);
+    double salary = atof(argv[3]);
+    fout << "Отчет по файлу " << argv[0] << endl;
+    fout << "Номер | Имя сотрудника | Часы | Зарплата" << endl;
+    while (fin >> emp.num)
+    {
+        fin >> emp.name >> emp.hours;
+        fout << emp.num << " | " << emp.name << " | " << emp.hours << " | " << emp.hours * salary << endl;
+    }
+}
